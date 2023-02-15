a86b63e7f9bb557de00e7de02eab13a027588134
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-02-15 05:30:15 +0200

    impliment categories functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,49 @@
 class CategoriesController < ApplicationController
   def index
-  end
-
-  def show
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to(categories_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to(categories_path)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @category = Category.find(params[:id])
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to(categories_path)
   end
   
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
